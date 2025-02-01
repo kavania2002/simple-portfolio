dc1f560188442f6c5a975c1ceb75712da0add4ee
Kavan Gondalia <kavan155gondalia@gmail.com>
2025-02-01 11:18:05 +0530

    feat: ✨ add inria sans font

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold">Hello World!</h1>
+      <h2 className="text-3xl font-bold font-inria-sans">Hello World!</h2>
     </>
   );
 }
