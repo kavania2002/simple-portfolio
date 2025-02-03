1142ef4a6dc2dbf2cbcdc36a1456c9e57e69f6c2
Kavan Gondalia <kavan155gondalia@gmail.com>
2025-02-04 01:25:24 +0530

    fix: 🐞 small overflow issue

--- src/sections/AboutMeSmallScreen.tsx
+++ src/sections/AboutMeSmallScreen.tsx
@@ -3,7 +3,7 @@ import Divider from "../components/Divider";
 
 const AboutMe = () => {
   return (
-    <div className="h-screen px-10 py-8">
+    <div className="h-screen px-10 py-6">
       <Divider />
       <div className="flex flex-col h-full justify-center gap-y-4">
         <p className="text-[6vh] leading-[6vh] font-bold text-tertiary">
